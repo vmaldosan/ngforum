b29756803ef3b7e266835c2b1d065a38b6d26fe8
vmaldosan <vmaldosan@gmail.com>
2017-03-27 16:07:18 +0100

    Fixes lint.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,13 +16,17 @@ import { Post } from './post';
 		<p>{{posts.length}} posts.</p>
 	`
 })
-export class AppComponent { 
-	title = 'NgForum'
+
+export class AppComponent {
+	title = 'NgForum';
 	postId = '1';
 	posts = [
-		new Post(1, 'Post 1', 'Lorem ipsum Magna sit aliquip cillum ad Ut officia eu Ut est reprehenderit consequat ut commodo Duis non dolor Excepteur nostrud.'),
-		new Post(2, 'Post 2', 'Lorem ipsum Magna sit aliquip cillum ad Ut officia eu Ut est reprehenderit consequat ut commodo Duis non dolor Excepteur nostrud.'),
-		new Post(3, 'Post 3', 'Lorem ipsum Magna sit aliquip cillum ad Ut officia eu Ut est reprehenderit consequat ut commodo Duis non dolor Excepteur nostrud.')
+		new Post(1, 'Post 1', `Lorem ipsum Magna sit aliquip cillum ad Ut officia eu 
+			Ut est reprehenderit consequat ut commodo Duis non dolor Excepteur nostrud.`),
+		new Post(2, 'Post 2', `Lorem ipsum Magna sit aliquip cillum ad Ut officia eu 
+			Ut est reprehenderit consequat ut commodo Duis non dolor Excepteur nostrud.`),
+		new Post(3, 'Post 3', `Lorem ipsum Magna sit aliquip cillum ad Ut officia eu 
+			Ut est reprehenderit consequat ut commodo Duis non dolor Excepteur nostrud.`)
 	];
 	myPost = this.posts[0];
 }
