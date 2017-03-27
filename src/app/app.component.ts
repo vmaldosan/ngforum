585e4d0c6e22ab1037b4b6fb885c8a8306f309ce
vmaldosan <vmaldosan@gmail.com>
2017-03-27 15:51:04 +0100

    Creates Post component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,28 @@
 import { Component } from '@angular/core';
+import { Post } from './post';
 
 @Component({
 	selector: 'my-app',
-	template: `<h1>Hello {{name}}</h1>`,
+	template: `
+		<h1>{{title}}</h1>
+		<h2>Selected post: {{postId}}</h2>
+		<p>Forum</p>
+		<ul>
+			<li *ngFor="let post of posts">
+				{{ post.subject }}
+				{{ post.content }}
+			</li>
+		</ul>
+		<p>{{posts.length}} posts.</p>
+	`
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent { 
+	title = 'NgForum'
+	postId = '1';
+	posts = [
+		new Post(1, 'Post 1', 'Lorem ipsum Magna sit aliquip cillum ad Ut officia eu Ut est reprehenderit consequat ut commodo Duis non dolor Excepteur nostrud.'),
+		new Post(2, 'Post 2', 'Lorem ipsum Magna sit aliquip cillum ad Ut officia eu Ut est reprehenderit consequat ut commodo Duis non dolor Excepteur nostrud.'),
+		new Post(3, 'Post 3', 'Lorem ipsum Magna sit aliquip cillum ad Ut officia eu Ut est reprehenderit consequat ut commodo Duis non dolor Excepteur nostrud.')
+	];
+	myPost = this.posts[0];
+}
---
+++ src/app/post.ts
@@ -0,0 +1,6 @@
+export class Post {
+	constructor(
+		public id: number,
+		public subject: string,
+		public content: string) { }
+}
